35a4558afe939f3e90f957324e9855a6509b6360
KenM4sters <EHCKenway@outlook.com>
2024-02-26 10:32:05 +0000

    removed files from sources.js

--- public/Experience/sources.js
+++ public/Experience/sources.js
@@ -1,20 +1,5 @@
 export default [
     {
-        name: 'rock_ao',
-        type: 'texture',
-        path: 'static/textures/Rock020_1K_AmbientOcclusion.jpg'
-    },
-    {
-        name: 'sky_1',
-        type: 'equirectangular',
-        path: 'static/textures/sky_1.hdr'
-    },
-    {
-        name: 'sky_2',
-        type: 'equirectangular',
-        path: 'static/textures/sky_2.hdr'
-    },
-    {
         name: 'sky_3',
         type: 'equirectangular',
         path: 'static/textures/sky_3.hdr'
